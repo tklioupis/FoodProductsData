aff19d225fd0956bec71d0a40113550a5bbb951e
Theodoros Lioupis <tklioupis@gmail.com>
2020-11-25 21:42:38 +0200

    Fisrt functional database creation, NOT RESTARTABLEgit add .
    If you re-run the code, you must delete the database items or the IDs given to the new records will be wrong

--- data.py
+++ data.py
@@ -11,32 +11,66 @@ import openfoodfacts
 import sqlite3
 import time
 
-#Creating the database
-conn = sqlite3.connect('FoodProducts.sqlite')
-cur = conn.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS Products
-    (id INTEGER UNIQUE, name TEXT, brand TEXT, barcode INTEGER UNIQUE)''')
-
 #Connection to the data source server
 login_session_object = openfoodfacts.utils.login_into_OFF()
 print("Connecting...")
-time.sleep(3)
+time.sleep(1)
+
+#Creating the database
+conn = sqlite3.connect('FoodProducts.sqlite')
+cur = conn.cursor()
+answer = input('Do you want to delete the existing Database? (yes or no): ')
+if answer == 'yes':
+    cur.execute('''DROP TABLE IF EXISTS Products''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Products
+    (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, brand TEXT, barcode INTEGER UNIQUE, countries TEXT, ingredient_tags TEXT)''')
     
 #Reading the data and storing them to the database
-for product in openfoodfacts.products.search_all({}):
-    #print(product.keys())
-    #break
+id = None
+count = 0
+cur.execute('SELECT max(id) FROM Products')
+try:
+    row = cur.fetchone()
+    if row is None :
+        id = 1
+    else:
+        id = row[0]
+except:
+    id = 1
+if id is None : id = 1
+
+print('Waiting to find unretreived data...')
+time.sleep(1)
+for product in openfoodfacts.products.search_all({}):   
     try:
         name = product['product_name']
         brand = product['brands']
         barcode = product['code']
         countries = product['countries']
-        intags = product['ingredients_analysis_tags']
+        intags = ''
+        for tag in product['ingredients_analysis_tags']:
+            intags += tag
+            intags += ' ' 
     except:
         continue
     print('--------------------------------NEW PRODUCT--------------------------------')
     print(name, brand, '('+ barcode + ')')
     print('Countries: ', countries)
     print(intags, '\n')
+    
+   
+    cur.execute('''INSERT OR IGNORE INTO Products (name, brand, barcode, countries, ingredient_tags)
+        VALUES ( ?, ?, ?, ?, ?)''', (name, brand, barcode, countries, intags))
+    count += 1
+    if count % 50 == 0: 
+        conn.commit()
+    if count % 100 == 0 : 
+        print('4 seconds pause')
+        time.sleep(4)
+    id += 1
+
+conn.commit()
+cur.close()
+
 
 
