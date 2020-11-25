cc2fc95ed47cb906efc8054be1889b094fbc04ae
Theodoros Lioupis <tklioupis@gmail.com>
2020-11-25 19:39:27 +0200

    Determined Product Data tags
    The code responsible for saving the data to the base is not ready yet

--- data.py
+++ data.py
@@ -1,14 +1,42 @@
+'''
+FoodProductsData project for Python For Everybody Online Course!
+
+Theodoros Lioupis
+tklioupis@gmail.com
+https://github.com/tklioupis
+https://www.linkedin.com/in/theodoros-lioupis-2225621a9/
+'''
+
 import openfoodfacts
+import sqlite3
+import time
 
+#Creating the database
+conn = sqlite3.connect('FoodProducts.sqlite')
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS Products
+    (id INTEGER UNIQUE, name TEXT, brand TEXT, barcode INTEGER UNIQUE)''')
+
+#Connection to the data source server
 login_session_object = openfoodfacts.utils.login_into_OFF()
-
+print("Connecting...")
+time.sleep(3)
+    
+#Reading the data and storing them to the database
 for product in openfoodfacts.products.search_all({}):
+    #print(product.keys())
+    #break
     try:
-        print('--------------------------------------------------------------------------')
-        print (product['product_name'])
-        print(product['brands'])
-        print('--------------------------------------------------------------------------')
+        name = product['product_name']
+        brand = product['brands']
+        barcode = product['code']
+        countries = product['countries']
+        intags = product['ingredients_analysis_tags']
     except:
         continue
+    print('--------------------------------NEW PRODUCT--------------------------------')
+    print(name, brand, '('+ barcode + ')')
+    print('Countries: ', countries)
+    print(intags, '\n')
 
 
